f7abf7f7fb4fb9307e650c7d54a3b05546f9a854
blerner5 <blerner5@student.touro.edu>
2025-10-26 12:19:51 -0400

    adjusted linked list code

--- StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
+++ StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
@@ -1,30 +1,57 @@
-}
+package linkedlist;
 
-// Step 2: Find cycle length
-int cycleLength = 1;
-        LLNode<T> current = slow.next;
+import support.LLNode;
+import support.CycleInfo;
+
+public class LinkedListCycleAnalyzer {
+
+    public static <T> CycleInfo detectCycleInfo(LLNode<T> head) {
+        if (head == null) {
+            return new CycleInfo(-1, 0);
+        }
+
+        LLNode<T> slow = head;
+        LLNode<T> fast = head;
+        boolean hasCycle = false;
+
+        // Step 1: Detect cycle
+        while (fast != null && fast.getLink() != null) {
+            slow = slow.getLink();
+            fast = fast.getLink().getLink();
+
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if (!hasCycle) {
+            return new CycleInfo(-1, 0);
+        }
+
+        // Step 2: Find cycle length
+        int cycleLength = 1;
+        LLNode<T> current = slow.getLink();
         while (current != slow) {
-        cycleLength++;
-        current = current.next;
+            cycleLength++;
+            current = current.getLink();
         }
 
         // Step 3: Find entry index
         LLNode<T> ptr1 = head;
         LLNode<T> ptr2 = head;
 
-        // Move ptr2 ahead by cycleLength steps
         for (int i = 0; i < cycleLength; i++) {
-        ptr2 = ptr2.next;
+            ptr2 = ptr2.getLink();
         }
 
         int entryIndex = 0;
         while (ptr1 != ptr2) {
-        ptr1 = ptr1.next;
-        ptr2 = ptr2.next;
-        entryIndex++;
+            ptr1 = ptr1.getLink();
+            ptr2 = ptr2.getLink();
+            entryIndex++;
         }
 
         return new CycleInfo(entryIndex, cycleLength);
     }
-            }
-            }
+}
